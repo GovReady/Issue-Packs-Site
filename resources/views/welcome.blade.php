4d5e6a350b7d98ccd5c5c697990b565f16d8ad37
Chris Birk <cmbirk@gmail.com>
2016-05-06 11:21:21 -0500

    add bootstrap,jquery js.  Clean up dashboard a bit.  Fix some gentelella styles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,11 @@
     </head>
     <body class="bg-faded nav-md pace-done">
       <div id="app"></div>
+      <!-- jQuery -->
+      <script src="https://code.jquery.com/jquery-2.2.3.min.js" integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo=" crossorigin="anonymous"></script>
+      <!-- Bootstrap JS -->
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+      <!-- Issue Packs Application -->
       <script src="/js/app.js"></script>
     </body>
 </html>
