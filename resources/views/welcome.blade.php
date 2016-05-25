13830a7d7e1f36e014afe702ccb3728c12a61c04
Chris Birk <cmbirk@gmail.com>
2016-05-25 11:29:10 -0500

    bump Auth0 lock version due to security vulnerability in the lock

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
       <title>Issue Packs</title>
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css">
       <link rel="stylesheet" href="/css/app.css">
-      <script src="//cdn.auth0.com/js/lock-9.0.min.js"></script>
+      <script src="//cdn.auth0.com/js/lock-9.1.min.js"></script>
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     </head>
     <body class="bg-faded pace-done">
