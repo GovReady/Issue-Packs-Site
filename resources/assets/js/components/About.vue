ddcbc35a6230f1beafb92ac1b30152b318c555f2
Chris Birk <cmbirk@gmail.com>
2016-06-29 01:17:23 -0500

    starting About page

--- resources/assets/js/components/About.vue
+++ resources/assets/js/components/About.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="about">
-
+    <div class="about-section">
+      <h4>How To Create An Issue Pack</h4>
+      <p>Issue Packs are simple <a href="http://yaml.org/" target="_blank">YAML</a> files that use the <a href="https://github.com/govready/issue-packs#issue-pack-format--yaml-" target="_blank">Issue Pack YAML Format.</a>  To create a new Issue Pack, simply open a text file with your favorite text editor and set up the pack name, issues, and issue labels as described in the <a href="https://github.com/govready/issue-packs#issue-pack-format--yaml-" target="_blank">Issue Pack Format</a> section of the library's README.  You're then free to upload your packs to <a v-link="{ name: 'my-packs' }">your packs page</a> and sync to any of your linked accounts.</p>
+    </div>
   </div>
 </template>
 <script></script>
